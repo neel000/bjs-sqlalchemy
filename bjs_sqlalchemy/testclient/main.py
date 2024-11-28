d64dde49eb72e7ed321e095d9da876ae82008ee8
Indranil Swarnakar <indranil.swarnakar@bjshomedelivery.com>
2024-11-28 20:59:00 +0530

    print statement remove

--- bjs_sqlalchemy/testclient/main.py
+++ bjs_sqlalchemy/testclient/main.py
@@ -1,7 +1,28 @@
 from bjs_sqlalchemy import models
-from bjs_sqlalchemy.models.config import DatabaseConfig, AsyncDatabaseConfig
 import unittest
 import asyncio
+from bjs_sqlalchemy.models import create_engine, sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+class TestDatabaseConfig:
+    _instance = None
+    def __new__(cls, db_url, debug=False):
+        if not cls._instance:
+            engine = create_engine(db_url, echo=debug)
+            Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+            cls._instance = Session()
+        return cls._instance
+
+class TestAsyncDatabaseConfig:
+    async def __new__(cls, db_url: str, debug: bool = False):
+        engine = create_async_engine(db_url, echo=debug)
+        AsyncSessionLocal = sessionmaker(
+            engine, class_=AsyncSession, expire_on_commit=False
+        )
+        
+        async with AsyncSessionLocal() as session:
+            async with session.begin():
+                return session
 
 class TestClient(unittest.TestCase):
     database_url = "sqlite:///./test.db"
@@ -11,12 +32,12 @@ class TestClient(unittest.TestCase):
     def setUpClass(cls):
         cls.engine = models.create_engine(cls.database_url)
         models.Base.metadata.create_all(cls.engine)
-        cls.session = DatabaseConfig(db_url=cls.database_url)
+        cls.session = TestDatabaseConfig(db_url=cls.database_url)
         cls.async_session = asyncio.run(cls.__async_session())
         
     @classmethod
     async def __async_session(cls):
-        return await AsyncDatabaseConfig(db_url=cls.asyn_database_url)
+        return await TestAsyncDatabaseConfig(db_url=cls.asyn_database_url)
 
     @classmethod
     async def close_session(cls):
